11b090762bb07603a9998dabf08a2a7825279fd0
Jai Gora <Jaigora24@gmail.com>
2021-08-03 02:22:33 +0530

    Update LinearSearch2.java

--- LinearSearch2.java
+++ LinearSearch2.java
@@ -1,14 +1,11 @@
 package searching_algo;
 public class LinearSearch2 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		int arr[] = new int [] {4,6,8,9,2,1,7,5,0,3};
 		int k = 8;
-		System.out.println(binarySearch(arr, k));
-		
+		System.out.println(binarySearch(arr, k));		
 		sc.close();		
-	}
-	
+	}	
 	public static int binarySearch(int [] a, int k) {
 		int found = -1;
 		for(int i=0; i<a.length; i++) {
@@ -17,7 +14,6 @@ public class LinearSearch2 {
 				break;
 			}
 		}
-	
 		return found;
 	}
 }
