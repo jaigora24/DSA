4d0428d0f7ea0bfe192383f31d433e39b2de98ff
Jai Gora <Jaigora24@gmail.com>
2021-08-08 01:05:44 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,29 @@
+package practise;
+public class main {	
+	public static void main(String[] args) {
+		
+		String s1 = "aba";
+		String s2 = "bba";
+		System.out.println(anagram(s1, s2));		
+	}
+	
+	public static boolean anagram(String a, String b) {
+		boolean isAnagram = false;
+		boolean visited[] = new boolean[b.length()];
+		if(a.length()==b.length()) {
+			for(int i=0; i<a.length(); i++) {
+				char c = a.charAt(i);
+				isAnagram = false;
+				for(int j=0; j<b.length(); j++) {
+					if(b.charAt(j) == c && !visited[j]) {
+						visited[j] = true;
+						isAnagram = true;
+						break;
+					}
+				}
+				if(!isAnagram) break;
+			}
+		}
+		return isAnagram;
+	}
+}
