90f07f18ab9700730232f46545b97148c8e69c24
Jai Gora <Jaigora24@gmail.com>
2021-08-23 00:58:10 +0530

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,42 @@
+package ds.Stack;
+
+public class Stack {
+	
+	private int maxSize;
+	private int[] stackArray;
+	private int top;
+	
+	public Stack(int size) {
+		this.maxSize = size;
+		this.stackArray = new int[maxSize];
+		this.top = -1;
+	}
+	public void push(int j) {
+		if(isFull()) System.out.println("Stack is full");
+		else {
+			top++;
+			stackArray[top]=j;
+		}
+	}
+	public long pop() {
+		if(isEmpty()) {
+			System.out.println("Stack is empty");
+			return  -1;
+		}
+		else {
+			int old_top = top;
+			top--;
+			return stackArray[old_top];
+		}
+	}
+	public long peek() {
+		return stackArray[top];
+	}
+	public boolean isFull() {
+		return (maxSize-1==top);
+	}
+	public boolean isEmpty() {
+		return (top==-1);
+	}
+	
+}
