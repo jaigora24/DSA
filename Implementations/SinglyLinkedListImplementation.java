2738796a04e37821c70e99d73abc1d59d75a6dcf
Jai Gora <Jaigora24@gmail.com>
2022-01-23 02:20:04 +0530

    SinglyLinkedListImplementation.java

--- Implementations/SinglyLinkedListImplementation.java
+++ Implementations/SinglyLinkedListImplementation.java
@@ -15,27 +15,56 @@ public class SinglyLinkedListImplementation{
 	int n=0;
 	
 	
-	//insert data in the beginning of the Linked List
-	public void insert_at_beginning(){
+	//insert data in the Linked List
+	public void insertion(){
 		Scanner sc = new Scanner(System.in);
 		
 		do{
+			System.out.println("Enter Data : ");
 			int data = sc.nextInt();
 			Node new_node = new Node(data);
+			Node temp = head;
 			
 			if(head==null){
 				head=new_node;
 			}
 			else{
-				new_node.next=head;
-				head=new_node;
+				
+				System.out.println("Press 1 to insert data at beginning \n Press 2 to insert data at end \n Press 3 to insert data at specific location");
+				System.out.println("Enter your choice : ");
+				int choice = sc.nextInt();
+				switch(choice){
+					case(1):
+						new_node.next=head;
+						head=new_node;
+						break;
+					case(2):
+						temp=head;
+						while(temp.next!=null){
+							temp=temp.next;
+						}
+						temp.next=new_node;
+						new_node.next=null;
+						break;
+					case(3):
+						System.out.println("Enter the position : ");
+						int position = sc.nextInt();
+						temp=head;
+						for(int i=1; i<position-1; i++){
+							temp=temp.next;
+						}
+						new_node.next=temp.next;
+						temp.next=new_node;
+						break;
+				}
+
+				
 			}
-			System.out.println("Press 1 if you want to add more data at the beginning");
+			System.out.println("Press 1 if you want to add more data");
 			n=sc.nextInt();
 		}
 		while(n==1);
 	}
-	
 	
 	//print data of the Linked List
 	public void print(){
@@ -54,7 +83,7 @@ public class SinglyLinkedListImplementation{
 	public static void main(String[] args){
 		
 		SinglyLinkedListImplementation ll = new SinglyLinkedListImplementation();
-		ll.insert_at_beginning();
+		ll.insertion();
 		ll.print();
 	}
 }
