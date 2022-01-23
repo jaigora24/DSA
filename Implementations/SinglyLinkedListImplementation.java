b7dc50c9c859c8318ede990bf6e0955fca86f7bf
Jai Gora <Jaigora24@gmail.com>
2022-01-24 00:01:23 +0530

    SinglyLinkedListImplementation.java

--- Implementations/SinglyLinkedListImplementation.java
+++ Implementations/SinglyLinkedListImplementation.java
@@ -66,6 +66,66 @@ public class SinglyLinkedListImplementation{
 		while(n==1);
 	}
 	
+	
+	
+	
+	//delete data from the Linked List
+	public void deletion(){
+		Scanner sc = new Scanner(System.in);
+		Node temp = head;
+		
+		do{
+			if(head==null){
+				System.out.println("Linked List is Empty");
+			}
+			else{
+				System.out.println("1. Beginning     2. Ending     3. Specific");
+				System.out.println("Enter your choice : ");
+				int choice = sc.nextInt();
+				
+				switch(choice){
+					case(1):
+						temp = head;
+						temp=temp.next;
+						head=temp;
+						break;
+					
+					case(2):
+						temp = head;
+						Node ptr = temp.next;
+						while(ptr.next!=null){
+							temp=ptr;
+							ptr=ptr.next;
+						}
+						temp.next=null;
+						break;
+					
+					case(3):
+						System.out.println("Enter the location where you want to add : ");
+						int loc = sc.nextInt();
+						
+						temp=head;
+						ptr=temp.next;
+						for(int i=1; i<loc-1; i++){
+							temp=ptr;
+							ptr=ptr.next;
+						}
+						temp.next=ptr.next;
+						break;
+					
+				}
+			}
+			System.out.println("Press 1. if you want to delete more data");
+			n = sc.nextInt();
+		}
+		while(n==1);
+	}
+	
+	
+	
+	
+	
+	
 	//print data of the Linked List
 	public void print(){
 		Node temp=head;
@@ -84,6 +144,7 @@ public class SinglyLinkedListImplementation{
 		
 		SinglyLinkedListImplementation ll = new SinglyLinkedListImplementation();
 		ll.insertion();
+		ll.deletion();
 		ll.print();
 	}
 }
