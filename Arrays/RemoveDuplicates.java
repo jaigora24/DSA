116757b3d6d2183cf0b94eab6cb92bb3551b1fb3
Jai Gora <Jaigora24@gmail.com>
2022-02-04 19:02:56 +0530

    Update RemoveDuplicates.java

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -4,29 +4,27 @@ public class main{
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int arr[] = new int[n];
-		int temp[] = new int[n];		
+		int a[] = new int[n];
 		
-		//input array
-		for(int i=0; i<arr.length; i++){
-			arr[i]=sc.nextInt();
+		for(int i=0; i<a.length; i++){
+			a[i]=sc.nextInt();
 		}
 		
-		//iterate in array & check if found duplicate then store duplicate in temp array
-		for(int i=0; i<arr.length; i++){
-			for(int j=i+1; j<arr.length; j++){
-				if(arr[i]==arr[j]){
-					temp[i]=arr[i];
+		int temp[] = new int[n];
+		
+		for(int i=0; i<a.length; i++){
+			for(int j=i+1; j<a.length; j++){
+				if(a[i]==a[j]){
+					temp[i]=a[j];
 				}
 			}
 		}
-		
-		//print only single element (not duplicate)
-		for(int i=0; i<n; i++){
-			int res = arr[i]-temp[i];
+		for(int i=0; i<a.length; i++){
+			int res = a[i]-temp[i];
 			if(res!=0){
 				System.out.print(res+" ");
 			}
 		}
+		
 	}
 }
