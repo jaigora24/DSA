118bbad3b61a60d45c63139851a118d7f5c4cddd
Jai Gora <Jaigora24@gmail.com>
2022-08-29 23:03:48 +0530

    Update DuplicateNumberInArray.java

--- Arrays/DuplicateNumberInArray.java
+++ Arrays/DuplicateNumberInArray.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
-public class DuplicateNumberInArray{
+import java.util.*;
+public class main{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		
-        // input array
 		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for(int i=0; i<n; i++){
-			arr[i]=sc.nextInt();
-		}
+		int a[] = new int[n];
+		boolean visited[] = new boolean[n];
 		
-        //outer loop iterate from 0 to array length & the inner loop will used to compare the selected element from rest of the array 
+		for(int i=0; i<n; i++){
+			a[i]=sc.nextInt();
+		}
 		for(int i=0; i<n; i++){
 			for(int j=i+1; j<n; j++){
-				if(arr[i]==arr[j]){
-					System.out.println(arr[j]);
+				if(a[i]==a[j] && !visited[j]){
+					visited[j]=true;
+					System.out.print(a[j]+" ");
 				}
 			}
 		}
